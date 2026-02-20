bf378c86361b66371752a0b79909ff05d21d5c97
GaurabCodeLab <kumargaurab56@gmail.com>
2026-02-20 20:02:27 +0530

    code changes

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -20,7 +20,7 @@ router.post("/signup", async (req, res) => {
     "gender",
   ];
   const isValidBody = Object.keys(newUserDetails).every((value) =>
-    userSchemaFields.includes(value)
+    userSchemaFields.includes(value),
   );
   if (!isValidBody) {
     return res.status(400).send("Invalid request data");
@@ -46,16 +46,16 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   if ((!email, !password)) {
-    return res.status(400).send("Missing required fields");
+    return res.status(400).json({ message: "Missing required fields" });
   }
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).send("Invalid credentials");
+      return res.status(401).json({ message: "Invalid credentials" });
     }
     const isValidPassword = await bcrypt.compare(password, user.password);
     if (!isValidPassword) {
-      return res.status(401).send("Invalid credentials");
+      return res.status(401).json({ message: "Invalid credentials" });
     }
     const token = jwt.sign({ id: user._id, email: user.email }, "Raja1634@", {
       expiresIn: "1h",
@@ -63,7 +63,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token);
     res.status(200).json({ message: "login successful", data: user });
   } catch (error) {
-    res.status(500).send("Error in login : " + error);
+    res.status(500).json({ message: "Error in login : " + error.message });
   }
 });
 
